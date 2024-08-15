7e44e32dce70b58b0ba8f939ce18616c81e8d4f0
TBS093A <zukkamil.44@gmail.com>
2024-08-15 19:46:48 +0200

    feat(kucoin api): trying different approach at sell
    -

--- api/kucoin.py
+++ api/kucoin.py
@@ -213,8 +213,8 @@ class KucoinAPI(
         return self._limit_order_request(
             transaction_side = "sell",
             coin = coin,
-            coin_size = symbol_lot_size["lotSize"],
-            coin_price = coin_sell_price,
+            coin_size = symbol_lot_size["base_min_size"],
+            coin_price = coin_percent_size_to_sell, #coin_sell_price,
             used_currency = used_currency,
             used_endpoint = self.__sell_endpoint
         )
