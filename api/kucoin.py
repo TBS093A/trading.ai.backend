924eeed899240615e6f01172c9fbe31abb45c959
TBS093A <zukkamil.44@gmail.com>
2024-08-17 11:54:06 +0200

    feat(kucoin api): prettyfi coin size availability output after sell
    -

--- api/kucoin.py
+++ api/kucoin.py
@@ -350,9 +350,12 @@ class KucoinAPI(
 
         pretty_sell_profit = float(ticker_data["price"]) * coin_sell_size
 
-        pretty_coin_size_availability_after_sell = self.get_available_currency_percent_price(
-            percent_size = 1.0,
-            currency = coin
+        pretty_coin_size_availability_after_sell = format(
+            self.get_available_currency_percent_price(
+                percent_size = 1.0,
+                currency = coin
+            ),
+            f".{len(str(int(1 / float(symbol_lot_size['base_increment']))))}f"
         )
 
         transaction_dict = dict(
