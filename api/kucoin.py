819da2b02a4cedbed5a9f5d83088eeefc3980e4f
TBS093A <zukkamil.44@gmail.com>
2024-08-17 11:56:57 +0200

    feat(kucoin api): fix float errors
    -

--- api/kucoin.py
+++ api/kucoin.py
@@ -351,9 +351,11 @@ class KucoinAPI(
         pretty_sell_profit = float(ticker_data["price"]) * coin_sell_size
 
         pretty_coin_size_availability_after_sell = format(
-            self.get_available_currency_percent_price(
-                percent_size = 1.0,
-                currency = coin
+            float(
+                self.get_available_currency_percent_price(
+                    percent_size = 1.0,
+                    currency = coin
+                )
             ),
             f".{len(str(int(1 / float(symbol_lot_size['base_increment']))))}f"
         )
