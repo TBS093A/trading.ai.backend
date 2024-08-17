df2875ac94a76ba0da661f5780e2584bdc2f0a01
TBS093A <zukkamil.44@gmail.com>
2024-08-17 14:38:32 +0200

    feat(kucoin api): parse floats
    -

--- api/kucoin.py
+++ api/kucoin.py
@@ -349,7 +349,10 @@ class KucoinAPI(
 
         print(f"\tcoin_high_limit_price ({coin_low_limit_price}) = ticker_data['price'] ({ticker_data['price']}) + price_percent_balance ({price_percent_balance})")
 
-        coin_price = coin_low_limit_price
+        coin_price = self.__truncate_float(
+            value = coin_low_limit_price,
+            precision = float(symbol_lot_size["price_increment"])
+        )
 
         print()
         print("sell:")
