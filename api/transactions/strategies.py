e968444f76339d818b55af6f2bb6113d9410d321
TBS093A <zukkamil.44@gmail.com>
2024-12-13 22:46:11 +0100

    style(strategies): change logging style -"-
    -

--- api/transactions/strategies.py
+++ api/transactions/strategies.py
@@ -330,7 +330,7 @@ class DistributedRiskStaticQuoteAndAssetTransactionStrategy(
 
             self.buy_transactions = int(available_quote / self.quote_currency_amount_per_transaction_used_to_sell)
 
-            print(f"Update Available Qoute  Used To Pump BUY Transactions (Possible Buy Transactions Count) Before Pump:\n\n{self.buy_transactions} x {self.quote_currency_amount_per_transaction_used_to_sell} {self.currency}\n")
+            print(f"Update Available Qoute ({available_quote} {self.currency}) Used To Pump BUY Transactions (Possible Buy Transactions Count) Before Pump -> {self.buy_transactions} x {self.quote_currency_amount_per_transaction_used_to_sell} {self.currency}")
 
     @StrategyUtils.elapsed_time
     async def _AbstractTransactionStrategy__buy_strategy(self, coin: str):
