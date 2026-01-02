10d1275d4db29e100e01d1a174be7ca4fd9730d0
TBS093A <zukkamil.44@gmail.com>
2026-01-02 10:40:52 +0100

    refactor(database): refactor database management
    refactor instructions in factory / facade / __init__.py in tables dir

--- src/db/postgresql/tables/__init__.py
+++ src/db/postgresql/tables/__init__.py
@@ -2,21 +2,7 @@ from .abstract_table import AbstractTable
 from .assets_table import AssetsTable
 from .exchanges_table import ExchangesTable
 from .asset_exchanges_table import AssetExchangesTable
-
-from .fundamental_analysis_table import FundamentalAnalysisTable
-from .fundamental_analysis_interpretation_table import FundamentalAnalysisInterpretationTable
 from .technical_analysis_harmonic_patterns_table import TechnicalAnalysisHarmonicPatternsTable
-from .technical_analysis_interpretation_table import TechnicalAnalysisInterpretationTable
-from .technical_analysis_interpretation_harmonic_patterns_table import TechnicalAnalysisInterpretationHarmonicPatternsTable
-from .technical_analysis_interpretation_chart_images_table import TechnicalAnalysisInterpretationChartImagesTable
-from .chart_images_table import ChartImagesTable
-from .chart_images_harmonic_patterns_table import ChartImagesHarmonicPatternsTable
-from .general_interpretation_table import GeneralInterpretationTable
-from .investment_strategies_table import InvestmentStrategiesTable
-from .exchange_transactions_table import ExchangeTransactionsTable
-from .exchange_account_state_table import ExchangeAccountStateTable
-from .exchange_account_state_buy_strategies_table import ExchangeAccountStateBuyStrategiesTable
-from .exchange_account_state_sell_strategies_table import ExchangeAccountStateSellStrategiesTable
 from .system_sync_job_table import SystemSyncJobTable
 from .cron_system_sync_job_table import CronSystemSyncJobTable
 
@@ -25,21 +11,7 @@ __all__ = [
     'AssetsTable',
     'ExchangesTable',
     'AssetExchangesTable',
-
-    'FundamentalAnalysisTable',
-    'FundamentalAnalysisInterpretationTable',
     'TechnicalAnalysisHarmonicPatternsTable',
-    'TechnicalAnalysisInterpretationTable',
-    'TechnicalAnalysisInterpretationHarmonicPatternsTable',
-    'TechnicalAnalysisInterpretationChartImagesTable',
-    'ChartImagesTable',
-    'ChartImagesHarmonicPatternsTable',
-    'GeneralInterpretationTable',
-    'InvestmentStrategiesTable',
-    'ExchangeTransactionsTable',
-    'ExchangeAccountStateTable',
-    'ExchangeAccountStateBuyStrategiesTable',
-    'ExchangeAccountStateSellStrategiesTable',
     'SystemSyncJobTable',
     'CronSystemSyncJobTable'
 ] 
